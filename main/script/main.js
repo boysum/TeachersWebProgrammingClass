88b575b3a740ec69f89ae82dd2311297cdc82fd2
Rob McGraw <rob.mcgraw@niceincontact.com>
2019-03-18 22:43:14 -0600

    Updated for the class.

--- main/script/main.js
+++ main/script/main.js
@@ -1,12 +1,36 @@
 var answer = generateAnswer(100);
+var textbox = document.querySelector('#textGuess');
+var button = document.querySelector('#buttonGuess');
 
+// Execute a function when the user releases a key on the keyboard
+textbox.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      button.click();
+    }
+  });
 
+var guessCount = 0;
 function guess(){
-    var textbox = document.querySelector('#textGuess');
+    textbox.select();
+    if(button.innerHTML === 'Play Again')
+    {
+        answer = generateAnswer(100);
+        button.innerHTML = 'Guess';
+        textbox.value = '';
+        guessCount = 0;
+        return;
+    }
+    guessCount++;
     var userGuess = textbox.value;
     if(userGuess == answer)
     {
-        alert('You win!');
+        button.innerHTML = 'Play Again'
+        alert('You win! You got it in ' + guessCount + ' guesses.');
+
     }
     else if(userGuess > answer)
     {
